24374fcab45d2fc2fa8bfe56f8d2f8cf55d7f87f
Lucky6400 <94078735+Lucky6400@users.noreply.github.com>
2021-11-22 00:28:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var dbox = document.getElementById('display-box');
+
+function getData(data){
+    dbox.style.borderColor = "grey";
+    dbox.value += data;
+}
+function backspace()  
+{  
+var exp = dbox.value;  
+dbox.value = exp.substring(0, exp.length - 1);  
+}  
+function calc(){
+    if (dbox.value != "") {
+        var a = dbox.value;
+        var a01 = eval(a);
+        var res = a + "=" + a01;
+        dbox.value = res;
+    } else {
+        dbox.style.borderColor = "red";
+    }
+}
